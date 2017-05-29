6d2184e5875229973d459115c57cd1cb8f4913c3
Ihtisham Shah <ihtisham.shah48@gmail.com>
2017-05-29 09:28:04 +0500

    camera added
    Added camera module

--- MobileApp/index.android.js
+++ MobileApp/index.android.js
@@ -12,46 +12,55 @@ import {
   View
 } from 'react-native';
 
-let test = fetch('http://192.168.100.61:8080').then((response) => console.log(response.json()))
-
+import Camera from 'react-native-camera';
 
 
 export default class MobileApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+         <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
       </View>
     );
   }
+   takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
 }
 
-const styles = StyleSheet.create({
+
+ 
+ const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+	
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+	width: 20,
+	height: 500,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
-
 AppRegistry.registerComponent('MobileApp', () => MobileApp);
